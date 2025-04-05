ab0fe14c6d46da0b77b1c2e6ebf4f533c1c4ee15
Eamon4075 <eamon.mahrooq@gmail.com>
2025-04-05 17:48:04 +0100

    Add files via upload
    I implemented both, but for Polymorphism, I only done Runtime Polymorphism because Compile Time Polymorphism requires overloaded operators to the derived class of Vehicle. Whoever has the task to do this you can decide what operators you need for the Car class (and any other if you have an idea for a derived class of Vehicle)

---
+++ Inheritance (Car inherits from Vehicle) and Runtime Polymorphism.cpp
@@ -0,0 +1,84 @@
+/*
+Inheritance between Vehicle and Car (Car inherits from Vehicle)
+I only implemented Runtime Polymorphism because:
+    For Compile Time Polymorphism you need to implement (whoever has the task to do so):
+		- Operator Overloading on the derived classes of Vehicle
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Vehicle {
+protected:
+    string make;
+    string model;
+    string color;
+    int maxSpeed;
+    static int vehicleCount;
+
+public:
+    Vehicle(string make, string model, string color, int maxSpeed)
+        : make(make), model(model), color(color), maxSpeed(maxSpeed) { vehicleCount++; }
+
+    virtual string getMake() const = 0;
+    virtual string getModel() const = 0;
+    virtual string getEngine() const = 0; // Polymorphic behaviour: pure virtual functions where the implementation will be in the derived class(es)
+    virtual string getType() const = 0;
+
+    virtual ~Vehicle() {}
+
+    
+    string getColor() const {
+        return color;
+    }
+
+    int getMaxSpeed() const {
+        return maxSpeed;
+    }
+
+    static int displayVehicleCount() {
+        return vehicleCount;
+    }
+};
+
+int Vehicle::vehicleCount = 0;
+
+class Car : public Vehicle {
+public:
+    Car(string make, string model, string color, int maxSpeed)
+        : Vehicle(make, model, color, maxSpeed) {}
+
+    string getMake() const override {
+        return make;
+    }
+
+    string getModel() const override {
+        return model;
+    }
+
+    string getEngine() const override {
+        return "1.5-liter three-cylinder petrol";
+    }
+
+    string getType() const override {
+        return "Hatchback";
+    }
+};
+
+int main() {
+    Car Vehicle1("Toyota", "Yaris", "Red", 109);  // Create a Car object on the stack
+    
+    // Runtime polymorphism where a pointer of type abstract base class is pointing to the concrete derived class(es)
+    Vehicle* v = &Vehicle1;  // Pointer to point to the address of the new object
+    cout << "Vehicle details: " 
+    << v->getMake() << " " 
+    << v->getModel() << ". "    // Polymorphic behaviour: change the address of the function that the pointer is pointing to
+    << v->getEngine() << endl;
+    
+    cout << "Type: " << v->getType() << endl;
+    
+    cout << "Vehicle object count: " << Vehicle::displayVehicleCount() << endl; // Print the vehicle count
+
+    return 0;
+}
