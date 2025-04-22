5d92be312e21a91da01adda40f7e19825a3f7c10
Eamon4075 <eamon.mahrooq@gmail.com>
2025-04-22 13:15:34 +0100

    Eamon update vehicle.cpp
    I found Adam's tasks, I added his name

--- F1_management_system/vehicle.cpp
+++ F1_management_system/vehicle.cpp
@@ -1,25 +1,32 @@
+// Responsible: Eamon Mahrooq
 // vehicle.cpp
 #include "vehicle.h"
 #include "driver.h"    // for Driver::setAssignedVehicle()
 
+// Responsible: Adam Bance
+// Initialize static count
 int Vehicle::vehicleCount = 0;
 
 Vehicle::Vehicle(const string& mk, const string& mdl, const string& col, int ms)
   : make(mk), model(mdl), color(col), maxSpeed(ms), assignedDriver(nullptr){
-    ++vehicleCount;
+    ++vehicleCount;                                 // Responsible: Adam Bance
+                                                    
 }
 
 Vehicle::~Vehicle(){
     --vehicleCount;
 }
 
+// Assign a Vehicle to a Driver
+// Responsible: Adam Bance
 void Vehicle::assignDriver(Driver* d){
     assignedDriver = d;
     if (d){
         d->setAssignedVehicle(this);
     }
 }
-
+// Get the Driver that was assigned the Vehicle
+// Responsible: Adam Bance
 Driver* Vehicle::getAssignedDriver() const{
     return assignedDriver;
 }
@@ -32,6 +39,7 @@ int Vehicle::getMaxSpeed() const{
     return maxSpeed;
 }
 
+// Responsible: Adam Bance
 int Vehicle::displayVehicleCount(){
     return vehicleCount;
 }
