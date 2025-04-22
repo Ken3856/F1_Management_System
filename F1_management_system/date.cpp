6dbd1d47cdc399ba19c391f7c4cf6b73d8bbba26
Eamon4075 <eamon.mahrooq@gmail.com>
2025-04-22 13:06:18 +0100

    Eamon updated date.cpp

--- F1_management_system/date.cpp
+++ F1_management_system/date.cpp
@@ -1,3 +1,4 @@
+// Responsible: Eamon Mahrooq
 #include "date.h"
 
 Date::Date() : day(0), month(0), year(0) {}
@@ -7,16 +8,19 @@ string Date::getDateString() const {
     return to_string(day) + "-" + to_string(month) + "-" + to_string(year);
 }
 
+// Responsible: Eamon Mahrooq
+// Output stream overload to print Date as string
 ostream& operator<<(ostream& os, const Date& d){
     os << d.getDateString();
     return os;
 }
 
+// Input stream overload to read Date in dd-mm-yy format
 istream& operator>>(istream& is, Date& d){
     int dd, mm, yy;
     char dash1, dash2;
     if (is >> dd >> dash1 >> mm >> dash2 >> yy){
-        d = Date(dd, mm, yy);
+        d = Date(dd, mm, yy); // Construct it
     }
     return is;
 }
