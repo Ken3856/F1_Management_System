bcb71db42f3d9daba98a30a9e24ee654bf672ee1
Eamon4075 <eamon.mahrooq@gmail.com>
2025-04-22 12:19:42 +0100

    Eamon updated engine.cpp

--- F1_management_system/engine.cpp
+++ F1_management_system/engine.cpp
@@ -1,9 +1,13 @@
+// Responsible: Eamon Mahrooq
 #include "engine.h"
 
+// Constructor
 Engine::Engine(const string& t, int hp): type(t), horsepower(hp){}
 
+// Destructor
 Engine::~Engine(){}
 
+// Getters
 string Engine::getType() const{
     return type;
 }
