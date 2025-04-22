48eff6e053bc157b40f5d93f9ba5149fd728574a
xiang6x <xyf630330@gmail.com>
2025-04-22 17:45:48 +0100

    raceQueue.cpp

--- F1_management_system/raceQueue.cpp
+++ F1_management_system/raceQueue.cpp
@@ -98,83 +98,92 @@ void RaceQueue:: showQueue(){
     }
     else{
         cout << "=== Race Queue ===" << endl;
-        RaceNode* temp = listhead;
+        RaceNode* temp = listhead;//  a pointer with type of RaceNode, points at listhead of queue, which is going to be shown
         int count = 1;
         while (temp != NULL){
-            cout << "\nRace # " << count << " :" << endl;
-            cout << "Event Name: " << temp->data.getName() << endl;
+            cout << "\nRace # " << count << " :" << endl;//first Race is number 1
+            cout << "Event Name: " << temp->data.getName() << endl;//as temp is a pointer, so use the arrow here
             cout << "Event Date: " << temp->data.getDate() << endl;
             cout << "Event Time: " << temp->data.getTime() << endl;
             cout << "Location: " << temp->data.getLocation() << endl; // Use getters instead of temp->data.showRaces();
-            temp = temp->next;
-            count++;
+            temp = temp->next;// move to the next
+            count++;//increase the num, make every nodes has a serial number
         }
     }
 }
 
 void RaceQueue:: deleteRaceNode(){
-    if (listhead == NULL){
+    if (listhead == NULL)//empty error checking
+    {
         cout << "The Queue is empty. Nothing to delete." << endl;
         return;
     }
     int total=0;
-    RaceNode* temp = listhead;
-    while (temp != NULL)
+    RaceNode* temp = listhead;// pointer "temp" point at listhead
+    while (temp != NULL)// here we want to get the total number of nodes in the queue
     {
-        total++;
-        temp = temp->next;
+        total++;//if current temp is not empty, we increacse the total by 1
+        temp = temp->next;// then move to next and go into cycle checking the next node
     }
-    int num;
-    showQueue();
+    int num;//initialization
+    showQueue(); // show the queue to who wants to modify it
 
     cout << "Which Race would you like to delete ? (choose number) " << endl;
     cin >> num;
-    if (cin.fail()) {
-        handleInvalidInputChar();
+    if (cin.fail())//if the input is not integer,cin.fail()would equals to true
+    {
+        handleInvalidInputChar();//call function from "utils.h"
         return;
     }
-    cin.ignore();
+    cin.ignore();//Cleans up what remains in the buffer when enter
 
 
-    if (num<1 || num>total){
+    if (num<1 || num>total)//check if input satified the range of queue
+    {
         cout << "error input,please choose a number between 1 and " << total << endl;
         return;
     }
 
-    string confirm;
+    string confirm;//initialization
     cout << "Are you sure you want to delete Race # " << num << " (yes/no)" << endl;
     cin >> confirm;
-    if (cin.fail()) {
+    if (cin.fail())//make sure the input is right type 
+    {
         handleInvalidInputChar();
         return;
     }
-    cin.ignore();
+    cin.ignore();// //Cleans up what remains in the buffer 
 
-    if (confirm != "Yes" && confirm != "yes" && confirm != "YES"){
+    if (confirm != "Yes" && confirm != "yes" && confirm != "YES")//check the right input
+    {
         cout << "deletion cancled " << endl;
-        return;
+        return;//if it's not "yes", not going to next step
     }
-    RaceNode* current = listhead;
-    RaceNode* prev = NULL;
+    RaceNode* current = listhead;//point at listhead
+    RaceNode* prev = NULL;//it's an empty pointer 
 
-    if (num == 1){
-        listhead = current->next;
-        delete current;
-        if (listhead == NULL){
-            listtail = NULL;
+    if (num == 1)//case of delete first node
+    {
+        listhead = current->next;//make next of listhead as new listhead
+        delete current;//pointer "curent" is original listhead, that's what we want to delete it
+        if (listhead == NULL)//just in case of this queue has only one node
+        {
+            listtail = NULL;// set the listtail empty
         }
-        cout << "Race # 1 has been deleted" << endl;
+        cout << "Race # 1 has been deleted" << endl;//delete succeed
         return;
     }
-    for (int i=1;i < num;i++){
-        prev = current;
-        current = current->next;
+    for (int i=1;i < num;i++)//now if the num is not 1
+    {
+        prev = current;//set current equals to prev, actually keep the "prev" always stay in the previous position of "current"
+        current = current->next;//move current to next, Stop by the number we want to delete
     }
-    prev->next = current->next;
-    if (current == listtail){
-        listtail = prev;
+    prev->next = current->next;//now connect the node before the target with the node after the target
+    if (current == listtail)//if "current" goes to end of queue
+    {
+        listtail = prev;//then after deleting the current, prev is our listtail
     }
-    delete current;
+    delete current;//delete the target node
 
-    cout << "Race # " << num << " has been deleted !" << endl;
+    cout << "Race # " << num << " has been deleted !" << endl;//success shown
 }
