4ce555870611c9bc95c724f2c2bfc25b13a83e3e
Ken <kenjohnson210@gmail.com>
2025-04-23 19:20:00 +0100

    Updated person_queue.cpp comments

--- F1_management_system/person_queue.cpp
+++ F1_management_system/person_queue.cpp
@@ -1,11 +1,18 @@
-#include "person_queue.h"
+// Responsible - Kenneth Johnson (Queue, inheritance, Abstract base class)
 
+#include "person_queue.h" // Includes the header file for PersonQueue class
+
+// Initialises an empty queue by setting the front and rear pointerr to nullptr.
 PersonQueue::PersonQueue() : front(nullptr), rear(nullptr) {}
 
+// Virtual  destructor for PersonQueue.
 PersonQueue::~PersonQueue(){
-    // base destructer does nothing, the derived classes clean up theirr Nodes
+    // This destructer does nothing, it doesnt delete nodes diretly,
+    // the derived classes DriverQueue and EngineerQueue clean up their Nodes.
+    // The virtual destructor is necessary for polymorphic memory cleanup.
 }
 
-bool PersonQueue::isEmpty() const{
-    return front == nullptr;
+// Checks if queue is empty.
+bool PersonQueue::isEmpty() const{ // Checks if the front pointer is nullptr, to see if no node are in the queue.
+    return front == nullptr; // returns trur if front is null, otherwise it returns false.
 }
