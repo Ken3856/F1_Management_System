4be7a924efba079268926c73d52cb87ea7b8ac4c
Ken <kenjohnson210@gmail.com>
2025-04-23 20:11:29 +0100

    Updated utils.h comments

--- F1_management_system/utils.h
+++ F1_management_system/utils.h
@@ -1,14 +1,18 @@
 // Utils.h
+
+// Header guard to revent multiple inclusions of th file.
 #ifndef INPUT_UTILS_H
 #define INPUT_UTILS_H
 
-#include <iostream>
-#include <limits>
+#include <iostream> // Include input/output stream 
 
-using namespace std;
+using namespace std; // Use namespace std to avoid using std prefix
 
 // ************ Display Banner & Utility ************
+// Displays ASCII "banner"
+// Uses "inline" to ensure that the function runs at runtime and prints the banner to the console screen.
 inline void displayBanner(){
+    // Uses raw string literal for ASCII art banner.
     cout << R"(
         _________   __  ___                                             __
        / ____<  /  /  |/  /___ _____  ____ _____ _____ ___  ___  ____  / /_
@@ -23,24 +27,27 @@ inline void displayBanner(){
     )" << '\n';
 }
 
+// Clears the screen and displays the banner again 
 inline void clearScreenDisplayBanner(){
     system("CLS"); // Clear screen (Windows)
-    displayBanner();
+    displayBanner(); // Calls displayBanner()
 }
 
+// This function pauses the screen and asks the user to press enter to continue.
 inline void pauseScreen(){
-    cout << "\nPress Enter to continue...";
+    cout << "\nPress Enter to continue..."; // Output message to screen.
     // cin.ignore();
-    cin.get();
+    cin.get(); // Waits for the user to press enter.
 }
 
+// Handles invalid characters being input. 
 inline void handleInvalidInputChar() {
     // Handle invalid input using a character
         cin.clear();  // Clear the error state
         cin.ignore(100, '\n');  // Discard invalid input up to the next newline
-        cout << "\nInvalid input. Please enter a number." << endl;
+        cout << "\nInvalid input. Please enter a number." << endl; // Outputs message to screen
         cout << "Press Enter to continue...";
-        cin.get();  // Wait for user to press Enter
+        cin.get();  // Wait for user to press Enter.
 }
 
 #endif // INPUT_UTILS_H
