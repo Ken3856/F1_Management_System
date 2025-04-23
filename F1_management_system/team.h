4d20f7855e18eaaab99e0b3280be2103b233cb6f
Ken <kenjohnson210@gmail.com>
2025-04-23 08:41:19 +0100

    Update team.h comments

--- F1_management_system/team.h
+++ F1_management_system/team.h
@@ -1,35 +1,45 @@
+// Responsible - Kenneth Johnson (Qualified Association)
+
+// Prevents multiple inclusions of this header file.
 #ifndef TEAM_H
 #define TEAM_H
 
-#include <string>
-#include <unordered_map>
+#include <string> // Include string library.
+#include <unordered_map> // Include unorderd map for mapping roles to pointers.
 
-using namespace std;
+using namespace std; // Use namespace std to avoid std prefix.
 
+// Forward declaration of classes.
 class Driver;
 class Engineer;
 
+// Class definition for Team class, which manages the qualified association.
 class Team{
 
+    // Private access specifier.
     private:
-    string name;
-        unordered_map<string, Driver*>   roleToDriverMap;
-        unordered_map<string, Engineer*> roleToEngineerMap;
-    
-        public:
+        string name;
+        unordered_map<string, Driver*>   roleToDriverMap; // Map from role (the qualifier) to Driver*
+        unordered_map<string, Engineer*> roleToEngineerMap; // Map from role (the qualifier) to Engineer*
+   
+    // Public access specifier.
+    public:
+        // Constructor to take a reference to a string 'n' to initialse the teams name.
         Team(const string& n);
+        // Destrutor that is called when a Team object is destroyed.
         ~Team();
-
-        // Qualified association for Drivers
-        void assignRoleToDriver(const string& role, Driver* d);
-        Driver* getDriverByRole(const string& role) const;
-
-        // Qualified association for Engineers
-        void assignEngineer(const string& role, Engineer* e);
-        Engineer* getEngineerByRole(const string& role) const;
-
+    
+        // Qualified association for Drivers. Qualify a Driver by a role.
+    
+        void assignRoleToDriver(const string& role, Driver* d); // Maps the Driver pointer 'd' into the teams driver role (key).
+        Driver* getDriverByRole(const string& role) const; // Looks up and returns the Driver* stored under the key (role) in the teams driver role map.
+    
+        // Qualified association for Engineers. Qualify an Engineer by a role.
+        void assignEngineer(const string& role, Engineer* e); // Maps the Engineer pointer 'e' into the teams engineer role map under the role (key).
+        Engineer* getEngineerByRole(const string& role) const; // Looks up and returns the Engineer* stored under the key (role) in the teams engineer role map.
+    
         // Getter
-        const string& getName() const;
+        const string& getName() const; // Returns a const reference to the stored string without copying it. Gets the Teams name. Called on a Team object.
 
 };
 
