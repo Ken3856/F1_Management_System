93fb578b76d577a02a4e8c68465731fafa1a279f
Ken <kenjohnson210@gmail.com>
2025-04-23 18:22:44 +0100

    Updated person_queue.h comments

--- F1_management_system/person_queue.h
+++ F1_management_system/person_queue.h
@@ -1,29 +1,40 @@
+// Responsible: Kenneth Johnson (Queue, Inheritance)
+
+// If not defined, then include a guard to prevent multiple inclusions.
 #ifndef PERSON_QUEUE_H
 #define PERSON_QUEUE_H
 
-#include "person.h"
-using namespace std;
+#include "person.h" // Include Person header file for Person class definition. 
+using namespace std; // Use namespace std to avoid std prefix.
 
-class PersonQueue {
-protected:
-    struct Node{
-        Person* person;
-        Node* next;
-        Node(Person* p) : person(p), next(nullptr) {}
-    };
+// Abstract base class for queue of Person objects.
+class PersonQueue{
+    // Protected access specifier, accessible inside the class and derived classes.
+    protected:
+        class Node{ // Nested Node class, as Node only exists to support PersonQueue.
+            Person* person; // Pointe to a person object.
+            Node* next; // Pointer to the next node in the queue;
 
-    Node* front;
-    Node* rear;
+            // Constructor with initialisation list for person pointer and setting next to nullptr.
+            Node(Person* p) : person(p), next(nullptr) {}
+        };
+    
+        Node* front // Pointer to the front node of the queue.
+        Node* rear; // Pointer to the rear node of the queue.
+    
+    // Public access specifier.
+    public:
+        // Default constructor to initialise an empty queue.
+        PersonQueue();
 
-public:
-    PersonQueue();
-    virtual ~PersonQueue(); // Let derived classes clean up
-
-    virtual void push(Person* p) = 0;
-    virtual void pop() = 0;
-    virtual void display() const = 0;
-
-    bool isEmpty() const;
+        // Virtual destructor to allow the derived classes to deallocate memory correctly for cleanup.
+        virtual ~PersonQueue(); // Let derived classes clean up
+    
+        virtual void push(Person* p) = 0; // A pure virtual function that adds a Person to the queue. ("=0" makes the function pure virtual).
+        virtual void pop() = 0; // A pure virtual function that removes a Person from the queue.
+        virtual void display() const = 0; // A pure virtual that displays the contents of the queue. (const so no member vairables are modified).
+    
+        bool isEmpty() const; // checks if the queue is empty.
 };
 
 #endif // PERSON_QUEUE_H
