c6c1dac088df40ff5a8e9cfdf1116fb57e3f15e8
Ken <kenjohnson210@gmail.com>
2025-04-23 16:03:11 +0100

    Update driver_queue.h comments

--- F1_management_system/driver_queue.h
+++ F1_management_system/driver_queue.h
@@ -1,16 +1,21 @@
+// Resposible - Kenneth Johnson (Driver Queue)
+
+// If not defined, define it to prevent multiple inclusions.
 #ifndef DRIVER_QUEUE_H
 #define DRIVER_QUEUE_H
 
-#include "person_queue.h"
+#include "person_queue.h" // Include the base PersonQueue class definition.
 
-using namespace std;
+using namespace std; // Use namespace std to avoid using std prefix.
 
-class DriverQueue : public PersonQueue {
+// DriverQueue inherits from PersonQueue to manage the driver objects in the queue.
+class DriverQueue : public PersonQueue{
 public:
-    ~DriverQueue();
-    void push(Person* p) override;
-    void pop() override;
-    void display() const  override;
+    ~DriverQueue(); // Destructor to clean up remaining nodes when out of scope.
+    void push(Person* p) override; // Pushes a person pointer onto queue. Overide is used for overiding the virtual base method.
+    void pop() override; // Deletes the front person i.e dequeues. Overide is used for overiding the virtual base method.
+    void display() const override;  // Displays all queued persons, const used to not modify the queue. Overide is used for overiding the virtual base method.
 };
 
+// End of the include guard 
 #endif // DRIVER_QUEUE_H
