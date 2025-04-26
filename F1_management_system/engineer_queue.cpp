a4dab38e29c2f2d920abd261d9a1e154fd5e71e6
Ken <kenjohnson210@gmail.com>
2025-04-26 23:27:35 +0100

    Update engineer_queue.cpp comments

--- F1_management_system/engineer_queue.cpp
+++ F1_management_system/engineer_queue.cpp
@@ -1,42 +1,46 @@
-#include "engineer_queue.h"
-#include <iostream>
+#include "engineer_queue.h" // Include header file for EngineerQueue and PersonQueue base class.
+#include <iostream> // Include input and output stream library.
 
-using namespace std;
+using namespace std; // Use namespace std to avoid std prefix.
 
+// Destructor, cleans nodes up and person objects.
 EngineerQueue::~EngineerQueue(){
-    while (!isEmpty()){
-        pop();
+    while (!isEmpty()){ // While queue isnt empty,
+        pop(); // traverse and remove each node.
     }
 }
 
+// Adds a new person pointer to end of queue.
 void EngineerQueue::push(Person* p){
-    Node* newNode = new Node(p);
-    if (!rear){
-        front = rear = newNode;
-    } else{
-        rear->next = newNode;
-        rear = newNode;
+    PersonNode* newNode = new PersonNode(p); // Creates a new node dynamically.
+    if (!rear){ // if the rear pointer is nullptr, i.e. queue is empty.
+        front = rear = newNode; // Set the front and rear to point to the new node.
+    } else{ // otherwise,
+        rear->next = newNode; // Set the current rear node to the new node.
+        rear = newNode; // And move the rear pointer to the new node.
     }
 }
 
+// Removes a person pointer from front of queue.
 void EngineerQueue::pop(){
-    if (isEmpty()){
-        return;
+    if (isEmpty()){ // If the queue is already empty,
+        return; // Just exit the fuction.
     }
-    Node* temp = front;
-    front = front->next;
-    delete temp->person;
-    delete temp;
-    if (!front){
-        rear = nullptr;
+    PersonNode* temp = front; // Stores the front node in temp.
+    front = front->next; // Moves the front pointer to the next node.
+    delete temp->person; //Delets the person object that the node was pointing to.
+    delete temp; // Deletes the node.
+    if (!front){ // If the queue is empty after popping,
+        rear = nullptr; // then reset the pointer to nullptr.
     }
 }
 
+//Displays the person information in the queue.
 void EngineerQueue::display() const{
-    Node* curr = front;
-    while (curr){
-        curr->person->displayInfo();
-        cout << endl;
-        curr = curr->next;
+    PersonNode* current = front; //Start at the front of queue.
+    while (current){ // Traverses through the queue until nullptr is reached.
+        current->person->displayInfo(); // Calls the display function of person. 
+        cout << endl; // Prints a new line.
+        current = current->next; // Moves to the next node within the queue.
     }
 }
